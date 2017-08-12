44f89364868e776060a83a7f03cd9df3e722f233
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-12 17:38:36 -0400

    did HTML mockup and CSS

---
+++ main.js
@@ -0,0 +1,6 @@
+let parent = document.querySelector("#search_results");
+
+let url = "https://recipe-puppy-proxy.herokuapp.com/api/recipes?q=";
+const button = document.querySelector("button");
+
+button.addEventListener("click", event => {});
