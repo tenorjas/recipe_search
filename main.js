e9cff31db62e68119abe85e1dbe12e90e7c4a9b7
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-12 18:05:52 -0400

    fetch is working

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ let parent = document.querySelector("#search_results");
 let url = "https://recipe-puppy-proxy.herokuapp.com/api/recipes?q=";
 const button = document.querySelector("button");
 
-button.addEventListener("click", event => {});
+button.addEventListener("click", event => {
+  console.log("Button has been clicked");
+
+  let searchBox = document.getElementById("searchBox");
+
+  const promise = fetch(url + searchBox.value).then(response => response.json()).then(recipe => {
+    console.log(recipe);
+  });
+});
