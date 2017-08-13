2295fd87816213b1f3c3872eca7e260725dd5ccc
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-12 20:02:11 -0400

    mostly working, except the screen doesn't clear between searches

--- main.js
+++ main.js
@@ -8,7 +8,43 @@ button.addEventListener("click", event => {
 
   let searchBox = document.getElementById("searchBox");
 
-  const promise = fetch(url + searchBox.value).then(response => response.json()).then(recipe => {
-    console.log(recipe);
+  const promise = fetch(url + searchBox.value).then(response => response.json()).then(recipes => {
+    console.log(recipes);
+
+    recipes.results.forEach((item, index) => {
+      let title = item.title;
+      let anchor = item.href;
+      let ingredients = item.ingredients;
+      let img = item.thumbnail;
+
+      let recipeBox = document.createElement("div");
+      recipeBox.className = "recipe";
+
+      let titleElement = document.createElement("h3");
+      titleElement.textContent = title;
+      let anchorElement = document.createElement("a");
+      anchorElement.href = anchor;
+      anchorElement.textContent = "Go to the recipe";
+      let ingredientsHeader = document.createElement("p");
+      ingredientsHeader.textContent = "Ingredients:";
+      let ingredientsList = document.createElement("p");
+      ingredientsList.textContent = ingredients;
+      let image = document.createElement("img");
+      if (img === "") {
+        // set to placeholder
+        image.src = "http://via.placeholder.com/200x200";
+      } else {
+        // use image in tag
+        image.src = img;
+      }
+
+      recipeBox.appendChild(titleElement);
+      recipeBox.appendChild(anchorElement);
+      recipeBox.appendChild(ingredientsHeader);
+      recipeBox.appendChild(ingredientsList);
+      recipeBox.appendChild(image);
+
+      parent.appendChild(recipeBox);
+    });
   });
 });
